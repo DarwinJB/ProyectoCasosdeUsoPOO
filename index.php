4b6b628d108b27e843096132e17ccebaa318d23f
julissa406 <54301347+julissa406@users.noreply.github.com>
2019-09-18 15:17:07 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+//enlace base de datos
+$conn_pgsql = "host=localhost port=5432 dbname=servidor1 user=postgres password=groot options'--client_encoding=UTF8";
+$ dbconn = pg_connect($conn_pgsql);
+
+if (!$dbconn) {
+	echo "Error de conexcion";
+} else {
+	echo "Conexion exitosa"
+
+}
+
 
 $usuario = $_POST['usuario'];
 $password = $_POST['password'];
